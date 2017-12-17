3de75f53390f1bdff0288d38f94d1dd2e18df19f
jinghuazhao <jinghua.zhao@mrc-epid.cam.ac.uk>
2017-12-17 09:33:28 +0000

    update

---
+++ st.sh
@@ -0,0 +1,20 @@
+# 17-12-2017 MRC-Epid JHZ
+
+git add .gitignore
+git commit -m "Don't process these"
+git add doc
+git commit -m "Documents and papers"
+git add SAS
+git commit -m "SAS programs"
+git add stata
+git add README.md
+git commit -m "README"
+git add DATA.md
+git commit -m "Data"
+git add REFERENCES.md
+git commit -m "References"
+git add SOFTWARE.*
+git commit -m "Software"
+git add vdi.md
+git commit -m "GWAS-2017.vdi"
+git push
